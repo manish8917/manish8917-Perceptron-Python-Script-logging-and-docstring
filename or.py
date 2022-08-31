d85d289a4f0c178dc1af3937a462960bcddd3c85
manish8917 <manishislampur1988@gmail.com>
2022-08-31 19:19:08 +0530

    logging updated

---
+++ or.py
@@ -0,0 +1,48 @@
+from utils.all_utils import prepare_data, save_plot
+from utils.model import Perceptron
+import pandas as pd
+import logging
+import os
+
+gate = "OR gate"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(
+    filename=os.path.join(log_dir, "running_logs.log"),
+    level=logging.INFO,
+    format='[%(asctime)s: %(levelname)s: %(module)s]: %(message)s',
+    filemode='a'
+    )
+
+
+def main(data, modelName, plotName, eta, epochs):
+    df = pd.DataFrame(data)
+    logging.info(f"This is the raw dataset: \n{df}")
+    X, y = prepare_data(df)
+
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X, y)
+
+    _ = model.total_loss()
+
+    model.save(filename=modelName, model_dir="model")
+    save_plot(df, model, filename=plotName)
+
+if __name__ == "__main__":
+    OR = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y" : [0,1,1,1]
+    }
+    ETA = 0.3
+    EPOCHS = 10
+    try:
+        logging.info(f">>>>> starting training for {gate}>>>>>")
+        main(data=OR, modelName="or.model", plotName="or.png", eta=ETA, epochs=EPOCHS)
+        logging.info(f"<<<<< done training for {gate} <<<<<\n\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
+
+
+
